d35517f3c746331abe418507f26619d6198e210b
erin <erin.nicole.johnson@gmail.com>
2019-12-04 20:09:20 +0000

    Create is_even.py

---
+++ is_even.py
@@ -0,0 +1,15 @@
+"""that will take a number x as input.
+
+If x is even, then return True.
+
+Otherwise, return False
+"""
+
+def is_even(x):
+    if x %2 == 0:
+        return True
+    else:
+        return False
+        
+        
+print is_even(900)
